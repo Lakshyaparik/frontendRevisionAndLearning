3a2b5ca78a3e9e78cadae43d430e7465edda7e43
lakshya <lakshyaparik99@gmail.com>
2026-01-02 22:21:16 +0530

    basic js

---
+++ JsRevision/day6/basic.js
@@ -0,0 +1,45 @@
+var a =10;
+//console.log(a);
+let b =20;
+//console.log(b);
+const c =30;
+//console.log(c);
+
+//logical operator
+
+// if(age <= 18 && isIndian)
+// {
+//   console.log('hi');
+  
+// }
+
+// let age = 17;
+
+// let result = age <=18 ? 'you are a minor' : 'you are an adult';
+
+// console.log(result);
+
+//for array
+
+// let arr = [10,20,30,40,50];
+
+// for (let value of arr){
+//     console.log(value);
+// }
+
+// let obj = {
+//     name : 'John',
+//     age : 25,
+//     city : 'New York'
+// }
+
+// for (let key in obj){
+//   console.log(obj[key]);
+  
+// }
+
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) continue;
+    if (i === 5) break;
+    console.log(i);
+}
