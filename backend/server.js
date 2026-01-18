f382036a1d278d8fda8fb96c7efc8b53d849bc84
lakshya <lakshyaparik99@gmail.com>
2026-01-18 14:42:20 +0530

    delted unused files

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-var slugify = require('slugify')
-
-let a=slugify('some string') // some-string
-console.log(a);
-
-
-// if you prefer something other than '-' as separator
-let b=slugify('some string', '=')  // some_string
-console.log(b);
