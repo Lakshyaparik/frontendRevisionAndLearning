34767fe8bb73c6df6690c4f6d0d645e3ed70dd3f
lakshya <lakshyaparik99@gmail.com>
2026-01-22 20:13:11 +0530

    revision of setup ejs project

---
+++ backend/v9/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+} );
+
+app.listen(3000, () => {
+   console.log('server is running on port 3000');
+   
+})
